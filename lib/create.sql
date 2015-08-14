ac739f5d630a7306a3aae37be47a631fd0d7c70e
Tyler Machen <tylermachen@gmail.com>
2015-08-14 10:40:15 -0400

    complete school, student, courses, enrollments tables

---
+++ lib/create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE schools(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  rating INTEGER,
+  year_established DATE
+);
+
+CREATE TABLE courses(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  school_id INTEGER REFERENCES schools
+);
+
+CREATE TABLE students(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE enrollments(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  student_id INTEGER REFERENCES students,
+  course_id INTEGER REFERENCES courses
+);
